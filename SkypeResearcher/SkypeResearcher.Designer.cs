4cd508ca68f3c1969a80fd2dcb05d48e32920ee7
Anton Yarkov <byslash@gmail.com>
2013-02-26 18:51:11 +0400

    Ok

---
+++ SkypeResearcher/SkypeResearcher.Designer.cs
@@ -0,0 +1,340 @@
+﻿namespace SkypeResearcher
+{
+    partial class SkypeResearcher
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.participants = new System.Windows.Forms.ComboBox();
+            this.usernamecb = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.statusMsg = new System.Windows.Forms.Label();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tabMessages = new System.Windows.Forms.TabPage();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.dataGridView4 = new System.Windows.Forms.DataGridView();
+            this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.dataGridView5 = new System.Windows.Forms.DataGridView();
+            this.tabPage6 = new System.Windows.Forms.TabPage();
+            this.dataGridView6 = new System.Windows.Forms.DataGridView();
+            this.tabPage7 = new System.Windows.Forms.TabPage();
+            this.dataGridView7 = new System.Windows.Forms.DataGridView();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabMessages.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.tabPage4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).BeginInit();
+            this.tabPage5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView5)).BeginInit();
+            this.tabPage6.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView6)).BeginInit();
+            this.tabPage7.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView7)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // participants
+            // 
+            this.participants.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.participants.FormattingEnabled = true;
+            this.participants.Location = new System.Drawing.Point(194, 43);
+            this.participants.Name = "participants";
+            this.participants.Size = new System.Drawing.Size(212, 21);
+            this.participants.TabIndex = 2;
+            // 
+            // usernamecb
+            // 
+            this.usernamecb.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.usernamecb.FormattingEnabled = true;
+            this.usernamecb.Location = new System.Drawing.Point(193, 14);
+            this.usernamecb.Name = "usernamecb";
+            this.usernamecb.Size = new System.Drawing.Size(212, 21);
+            this.usernamecb.Sorted = true;
+            this.usernamecb.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(11, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Select skype account:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(161, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Select contact to see messages:";
+            // 
+            // statusMsg
+            // 
+            this.statusMsg.AutoSize = true;
+            this.statusMsg.Location = new System.Drawing.Point(12, 238);
+            this.statusMsg.Name = "statusMsg";
+            this.statusMsg.Size = new System.Drawing.Size(0, 13);
+            this.statusMsg.TabIndex = 8;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(556, 357);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Tables main.db";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(6, 6);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(544, 345);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // tabMessages
+            // 
+            this.tabMessages.Controls.Add(this.dataGridView2);
+            this.tabMessages.Location = new System.Drawing.Point(4, 22);
+            this.tabMessages.Name = "tabMessages";
+            this.tabMessages.Size = new System.Drawing.Size(556, 357);
+            this.tabMessages.TabIndex = 2;
+            this.tabMessages.Text = "Messages";
+            this.tabMessages.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(6, 6);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(544, 316);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Controls.Add(this.tabPage5);
+            this.tabControl1.Controls.Add(this.tabPage6);
+            this.tabControl1.Controls.Add(this.tabPage7);
+            this.tabControl1.Controls.Add(this.tabMessages);
+            this.tabControl1.Location = new System.Drawing.Point(15, 70);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(564, 383);
+            this.tabControl1.TabIndex = 10;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.dataGridView3);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Size = new System.Drawing.Size(556, 357);
+            this.tabPage2.TabIndex = 3;
+            this.tabPage2.Text = "Tables bistats.db";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(6, 6);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.Size = new System.Drawing.Size(544, 316);
+            this.dataGridView3.TabIndex = 1;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Controls.Add(this.dataGridView4);
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Size = new System.Drawing.Size(556, 357);
+            this.tabPage4.TabIndex = 4;
+            this.tabPage4.Text = "Tables dc.db";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView4
+            // 
+            this.dataGridView4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView4.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView4.Location = new System.Drawing.Point(6, 6);
+            this.dataGridView4.Name = "dataGridView4";
+            this.dataGridView4.Size = new System.Drawing.Size(544, 316);
+            this.dataGridView4.TabIndex = 1;
+            // 
+            // tabPage5
+            // 
+            this.tabPage5.Controls.Add(this.dataGridView5);
+            this.tabPage5.Location = new System.Drawing.Point(4, 22);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Size = new System.Drawing.Size(556, 357);
+            this.tabPage5.TabIndex = 5;
+            this.tabPage5.Text = "Tables eas.db";
+            this.tabPage5.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView5
+            // 
+            this.dataGridView5.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView5.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView5.Location = new System.Drawing.Point(6, 6);
+            this.dataGridView5.Name = "dataGridView5";
+            this.dataGridView5.Size = new System.Drawing.Size(544, 316);
+            this.dataGridView5.TabIndex = 1;
+            // 
+            // tabPage6
+            // 
+            this.tabPage6.Controls.Add(this.dataGridView6);
+            this.tabPage6.Location = new System.Drawing.Point(4, 22);
+            this.tabPage6.Name = "tabPage6";
+            this.tabPage6.Size = new System.Drawing.Size(556, 357);
+            this.tabPage6.TabIndex = 6;
+            this.tabPage6.Text = "Tables keyval.db";
+            this.tabPage6.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView6
+            // 
+            this.dataGridView6.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView6.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView6.Location = new System.Drawing.Point(6, 6);
+            this.dataGridView6.Name = "dataGridView6";
+            this.dataGridView6.Size = new System.Drawing.Size(544, 316);
+            this.dataGridView6.TabIndex = 1;
+            // 
+            // tabPage7
+            // 
+            this.tabPage7.Controls.Add(this.dataGridView7);
+            this.tabPage7.Location = new System.Drawing.Point(4, 22);
+            this.tabPage7.Name = "tabPage7";
+            this.tabPage7.Size = new System.Drawing.Size(556, 357);
+            this.tabPage7.TabIndex = 7;
+            this.tabPage7.Text = "Tables msn.db";
+            this.tabPage7.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView7
+            // 
+            this.dataGridView7.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView7.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView7.Location = new System.Drawing.Point(6, 6);
+            this.dataGridView7.Name = "dataGridView7";
+            this.dataGridView7.Size = new System.Drawing.Size(544, 316);
+            this.dataGridView7.TabIndex = 1;
+            // 
+            // SkypeResearcher
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(583, 465);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.statusMsg);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.participants);
+            this.Controls.Add(this.usernamecb);
+            this.Name = "SkypeResearcher";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Skype Researcher";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabMessages.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.tabPage4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).EndInit();
+            this.tabPage5.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView5)).EndInit();
+            this.tabPage6.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView6)).EndInit();
+            this.tabPage7.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView7)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox participants;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox usernamecb;
+        private System.Windows.Forms.Label statusMsg;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TabPage tabMessages;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.TabPage tabPage5;
+        private System.Windows.Forms.TabPage tabPage6;
+        private System.Windows.Forms.TabPage tabPage7;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.DataGridView dataGridView4;
+        private System.Windows.Forms.DataGridView dataGridView5;
+        private System.Windows.Forms.DataGridView dataGridView6;
+        private System.Windows.Forms.DataGridView dataGridView7;
+    }
+}
+
---
+++ SkypeResearcher/SkypeResearcher.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SQLite;
+using System.Diagnostics;
+using System.IO;
+
+namespace SkypeResearcher
+{
+    public partial class SkypeResearcher : Form
+    {
+        public SkypeResearcher()
+        {
+            InitializeComponent();
+
+            _dirName = Environment.GetEnvironmentVariable("APPDATA") + @"\Skype";
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (Directory.Exists(_dirName))
+            {
+                _RetrieveInfoFromSkypeFolder();
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private void _RetrieveInfoFromSkypeFolder()
+        {
+            try
+            {
+                if (Directory.Exists(_dirName))
+                {
+                    string[] folders = Directory.GetDirectories(_dirName);
+
+                    foreach (string fname in folders)
+                    {
+                        string folder = fname.Replace(_dirName, string.Empty).Trim('\\');
+
+                        if (folder != "Content" &&
+                            folder != "My Skype Received Files" &&
+                            folder != "Pictures" &&
+                            folder != "DbTemp")
+                        {
+                            usernamecb.Items.Add(folder);
+                        }
+                    }
+
+                    if (usernamecb.Items.Count > 0)
+                    {
+                        usernamecb.SelectedIndex = -1;
+                        usernamecb.SelectedIndexChanged += usernamecb_SelectedIndexChanged;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void usernamecb_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (usernamecb.SelectedIndex > -1)
+            {
+                string skypeUserName = usernamecb.Items[usernamecb.SelectedIndex].ToString();
+                string database = _dirName + @"\" + skypeUserName;
+
+                string main = Application.UserAppDataPath + @"\main.db";
+                string bistats = Application.UserAppDataPath + @"\bistats.db";
+                string dc = Application.UserAppDataPath + @"\dc.db";
+                string eas = Application.UserAppDataPath + @"\eas.db";
+                string keyval = Application.UserAppDataPath + @"\keyval.db";
+                string msn = Application.UserAppDataPath + @"\msn.db";
+
+                File.Copy(database + @"\main.db", main, true);
+                File.Copy(database + @"\bistats.db", bistats, true);
+                File.Copy(database + @"\dc.db", dc, true);
+                File.Copy(database + @"\eas.db", eas, true);
+                File.Copy(database + @"\keyval.db", keyval, true);
+                File.Copy(database + @"\msn.db", msn, true);
+
+                if (File.Exists(main))
+                {
+                    _FillContacts(main);
+
+                    dataGridView1.DataSource = _GetTablesInfo(dataGridView1, main);
+                    dataGridView3.DataSource = _GetTablesInfo(dataGridView1, bistats);
+                    dataGridView4.DataSource = _GetTablesInfo(dataGridView1, dc);
+                    dataGridView5.DataSource = _GetTablesInfo(dataGridView1, eas);
+                    dataGridView6.DataSource = _GetTablesInfo(dataGridView1, keyval);
+                    dataGridView7.DataSource = _GetTablesInfo(dataGridView1, msn);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void participants_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (usernamecb.SelectedIndex > -1 && participants.SelectedIndex > -1)
+            {
+                string skypeUserName = usernamecb.SelectedItem.ToString();
+                string participant = participants.SelectedValue.ToString();
+                string database = _dirName + @"\" + skypeUserName + @"\main.db";
+
+                if (File.Exists(database))
+                {
+                    SQLiteConnection sqlite = new SQLiteConnection("data source=" + database);
+                    SQLiteDataAdapter ad;
+                    DataTable dt = new DataTable();
+                    SQLiteCommand cmd;
+
+                    try
+                    {
+                        sqlite.Open();
+                        cmd = sqlite.CreateCommand();
+                        // 'guid' field dropped since Exception
+                        cmd.CommandText = @"select id,is_permanent,convo_id,chatname,author,from_dispname,author_was_live,dialog_partner,
+                        timestamp,type,sending_status,consumption_status,edited_by,edited_timestamp,param_key,param_value,
+                        body_xml,identities,reason,leavereason,participant_count,error_code,chatmsg_type,chatmsg_status,
+                        body_is_rawxml,oldoptions,newoptions,newrole,pk_id,crc,remote_id,call_guid,extprop_contact_review_date,
+                        extprop_contact_received_stamp,extprop_contact_reviewed
+                        from Messages Where dialog_partner = '"
+                            + participant + "'";
+                        ad = new SQLiteDataAdapter(cmd);
+                        ad.Fill(dt);
+
+                        dataGridView2.DataSource = dt;
+                        tabControl1.SelectedTab = tabControl1.TabPages["tabMessages"];
+                    }
+                    catch (Exception)
+                    {
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="database"></param>
+        private void _FillContacts(string database)
+        {
+            SQLiteConnection sqlite = new SQLiteConnection("data source=" + database);
+
+            try
+            {
+                sqlite.Open();
+
+                //
+                SQLiteCommand cmd = sqlite.CreateCommand();
+                cmd.CommandText = "select DISTINCT dialog_partner from Messages;";
+
+                SQLiteDataAdapter ad = new SQLiteDataAdapter(cmd);
+
+                DataTable dt = new DataTable();
+                ad.Fill(dt);
+
+                participants.DataSource = dt;
+                participants.ValueMember = "dialog_partner";
+
+                participants.SelectedIndex = -1;
+                participants.SelectedIndexChanged += participants_SelectedIndexChanged;
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        void f()
+        {
+string path = "[PATH_TO_USERDATA]";
+DataTable dt = new DataTable();
+SQLiteConnection sqlite = new SQLiteConnection("data source=" + path);
+sqlite.Open();
+
+SQLiteDataAdapter ad;
+SQLiteCommand cmd = sqlite.CreateCommand();
+cmd.CommandText = @"select * from Messages";
+
+ad = new SQLiteDataAdapter(cmd);
+ad.Fill(dt);
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="dataGridView"></param>
+        /// <param name="database"></param>
+        /// <returns></returns>
+        private DataTable _GetTablesInfo(DataGridView dataGridView, string database)
+        {
+            DataTable dt = new DataTable();
+            SQLiteConnection sqlite = new SQLiteConnection("data source=" + database);
+
+            try
+            {
+                sqlite.Open();
+
+                //
+                SQLiteCommand cmd = sqlite.CreateCommand();
+                cmd.CommandText = "SELECT * FROM sqlite_master WHERE type='table';";
+
+                SQLiteDataAdapter ad = new SQLiteDataAdapter(cmd);
+
+                ad.Fill(dt);
+            }
+            catch (Exception)
+            {
+            }
+
+            return dt;
+        }
+
+        private string _dirName;
+    }
+}
